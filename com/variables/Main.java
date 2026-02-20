49b18a4c27fb4e9a8de4bd656b5e3f0de206ce16
Lakshman <laxmannani960@gmail.com>
2026-02-20 14:23:47 +0530

    first task

---
+++ com/variables/Main.java
@@ -0,0 +1,52 @@
+package com.variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String name = "Lakshman";
+			int age = 21;
+			String grade = "A+";
+			System.out.println(name);
+			System.out.println(age);
+			System.out.println(grade);
+			int count = 25;
+			System.out.println(count);
+			count = 46;
+			System.out.println(count);
+			byte num = 20;
+			Short num2 = 12;
+			int num3 = 25;
+			double num4 = 23.223;
+			float num5 = 21;
+			char ch = 'l';
+			boolean isStudent = false;
+			
+			
+			int a = 25;
+			int b = 50;
+			System.out.println(a);
+			System.out.println(b);
+			int temp = a;
+			a = b;
+			b = temp;
+			System.out.println(a);
+			System.out.println(b);
+			
+			
+			int d = 250;
+			int f = 500;
+			System.out.println(d);
+			System.out.println(f);
+			d = d ^ f;
+			f = d ^ f;
+			d = d ^ f;
+			System.out.println(d);
+			System.out.println(f);
+			
+			
+			final  double pi = Math.PI;
+			
+	}
+
+}
